6f4818cc7b59143ddabc4ae45ee81bf2d05d8554
sanarion <moristolp@hotmail.co.jp>
2020-03-20 21:16:48 +0900

    03-11-7end

--- example/ch03/ex03-11-5.js
+++ example/ch03/ex03-11-5.js
@@ -3,3 +3,31 @@ const sam1 = {
   age: 4
 };
 console.log(sam1);
+
+const sam2= {name: 'Sam', age: 4};
+console.log(sam1.name === sam2.name);
+console.log(sam1.age===sam2.age);
+console.log(sam1===sam2);
+console.log(sam1==sam2);
+
+const sam3 = {
+  name:'Sam',
+  classification: {
+    kingdom:'Animalia',
+    plylum:'Chordata'
+  }
+};
+console.log(sam3);
+
+console.log(sam3.classification.kingdom);
+console.log(sam3["classification"].kingdom);
+console.log(sam3.classification["kingdom"]);
+console.log(sam3["classification"]["kingdom"]);
+
+sam3.speak = function () {return "Meow!"};
+sam3.speak2 = () => {return "MeowMeow!"};
+sam3.speak3 = () => "MeowMeowMeow";
+
+console.log(sam3.speak());
+console.log(sam3.speak2());
+console.log(sam3.speak3());
