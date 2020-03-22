e5105fbeee0e0b8bdc1bb434fa4a05209aa4dbb5
sanarion <moristolp@hotmail.co.jp>
2020-03-22 18:48:38 +0900

    button created

---
+++ sample-code/calc/calc.js
@@ -0,0 +1,20 @@
+let inputLabel = document.getElementById('inputLabel');
+
+let pushBtn = (obj) => {
+  let pushed = obj.innerHTML;
+
+  if (pushed === "=") {
+    inputLabel.innerHTML = eval(inputLabel.innerHTML);
+  }
+  else if (pushed === "AC") {
+    inputLabel.innerHTML = "0";
+  }
+  else {
+    if (inputLabel.innerHTML === "0") {
+      inputLabel.innerHTML = pushed;
+    }
+    else {
+      inputLabel.innerHTML += pushed;
+    }
+  }
+}
