bdc023e8bb14e479fe85aa6a26fc03998aaa666a
sanarion <moristolp@hotmail.co.jp>
2020-04-12 17:04:53 +0900

    javascript end

---
+++ example/ch20/app.js
@@ -0,0 +1,4 @@
+const amanda = require('./amanda.js');
+const tyle_calculate = require('./tyler.js');
+
+console.log(amanda.arithmeticSum(5));
---
+++ example/ch20/debug-test.js
@@ -0,0 +1,7 @@
+const debug = require('debug')('main');
+debug('start');
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+debug('end');
